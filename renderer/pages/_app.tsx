b79e5d1de08ac6784ca4ddbd9090c10e44b4ed5c
Som S. <somwrks@gmail.com>
2024-05-28 22:05:33 +0530

    Workin

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -8,20 +8,31 @@ import {
 } from "@clerk/nextjs";
 
 import '../styles/globals.css'
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
-<ClerkProvider >
+  
+return(
+   <ClerkProvider >
       <SignedOut>
         <html>
           <body>
-            <SignInButton />
+            <div className="flex flex-col w-full min-h-screen items-center justify-center">
+            <SignInButton>
+              <div className='text-xl cursor-pointer p-3 rounded-2xl bg-white text-blue-600 '>
+               
+                Sign In
+              </div>
+              </SignInButton>
+            </div>
           </body>
         </html>
       </SignedOut>
       <SignedIn>
       <Component {...pageProps} />
-      </SignedIn>
+       </SignedIn>
     </ClerkProvider>
+)
 }
 
 export default MyApp
