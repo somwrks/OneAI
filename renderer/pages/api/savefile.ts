5f52b4a7b79460c912254edfbba01d7d442743a4
Som S. <somwrks@gmail.com>
2024-06-28 22:26:08 +0530

    Workin

--- renderer/pages/api/savefile.ts
+++ renderer/pages/api/savefile.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 
-function createReadme(data: any[], dir: string, title: string) {
+function createReadmeContent(data: any[], title: string) {
   let content = "";
 
   const sectionOrder = [
@@ -29,6 +29,10 @@ function createReadme(data: any[], dir: string, title: string) {
   content = content.replace("# Title", `# ${title}`);
   console.log("Generated Content:", content);
 
+  return content;
+}
+
+function writeReadmeToFile(content: string, dir: string) {
   fs.writeFile(dir + "/readme.md", content, (err) => {
     if (err) {
       console.error("Error writing file:", err);
@@ -41,8 +45,20 @@ function createReadme(data: any[], dir: string, title: string) {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const { data, dir, title } = req.body;
-    createReadme(data, dir, title);
-    res.status(200).json({ message: "Readme file created successfully!" });
+    const isGithub = dir.includes("https://github.com");
+
+    const readmeContent = createReadmeContent(data, title);
+
+    if (isGithub) {
+      // Send the generated content as a response for the user to download
+      res.setHeader("Content-Disposition", "attachment; filename=README.md");
+      res.setHeader("Content-Type", "text/markdown");
+      res.status(200).send(readmeContent);
+    } else {
+      // If it's not a GitHub URL, create the readme file in the local directory
+      writeReadmeToFile(readmeContent, dir);
+      res.status(200).json({ message: "Readme file created successfully!" });
+    }
   } else {
     res.status(405).json({ error: "Method Not Allowed" });
   }
